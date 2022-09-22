2475885af19bc24d78376c06b203d0df91337944
niicode <niidarku66@gmail.com>
2022-09-22 12:39:17 +0000

    Removed unwanted files

--- src/index.js
+++ src/index.js
@@ -4,7 +4,6 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import { Provider } from 'react-redux';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 import HomePage from './pages/Homepage';
 import Categories from './pages/Categories';
 import rootReducer from './redux/configureStore';
@@ -24,8 +23,3 @@ root.render(
     </BrowserRouter>
   </Provider>,
 );
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
